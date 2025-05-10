91ddff61a92ca280c0af40093f71331e2d29b339
aleksat <aleksat@twodesperados.com>
2025-05-10 16:53:16 +0200

    On score changed logic for progress bar

--- Assets/Scripts/UI/PlayerScoreBar.cs
+++ Assets/Scripts/UI/PlayerScoreBar.cs
@@ -8,4 +8,40 @@ public class PlayerScoreBar : MonoBehaviour
 {
     [SerializeField] private Slider playerScoreSlider;
     [SerializeField] private Image playerIcon;
+    [SerializeField] private float lerpDuration = 0.5f;
+
+    private Coroutine lerpRoutine;
+
+    private void Start()
+    {
+        playerScoreSlider.maxValue = App.Instance.GameSettings.MaxPossibleScore;
+    }
+
+    public void OnScoreChanged(int newScore)
+    {
+        if (lerpRoutine != null)
+            StopCoroutine(lerpRoutine);
+
+        if (lerpRoutine != null)
+            StopCoroutine(lerpRoutine);
+        
+        lerpRoutine = StartCoroutine(LerpScore(newScore));
+    }
+
+    private IEnumerator LerpScore(int targetScore)
+    {
+        float startValue = playerScoreSlider.value;
+        float time = 0f;
+
+        while (time < lerpDuration)
+        {
+            time += Time.deltaTime;
+            float t = time / lerpDuration;
+            playerScoreSlider.value = Mathf.Lerp(startValue, targetScore, t);
+            yield return null;
+        }
+
+        playerScoreSlider.value = targetScore;
+        lerpRoutine = null;
+    }
 }
