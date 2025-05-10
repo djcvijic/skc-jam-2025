2b3275ffd95d4ce83809545b7095fdc70571d39a
aleksat <aleksat@twodesperados.com>
2025-05-10 10:42:13 +0200

    Prepared app canvas and event notifier scripts

--- Assets/Scripts/App.cs
+++ Assets/Scripts/App.cs
@@ -7,7 +7,9 @@ public class App : MonoBehaviour
 {
     public static App Instance;
     public GameSettings GameSettings;
-
+    
+    [field: SerializeField] public AppCanvas AppCanvas { get; private set; }
+    
     private void Start()
     {
         Instance = this;
---
+++ Assets/Scripts/EventsNotifier.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class EventsNotifier
+{
+    public event Action<bool> EventExample;
+
+    public void NotifyGraceChanged(bool value)
+        => EventExample?.Invoke(value);
+}
