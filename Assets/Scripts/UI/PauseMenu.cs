70d8ff27f77ec5998d48113f38f539d74da67baa
aleksat <aleksat@twodesperados.com>
2025-05-11 17:38:42 +0200

    Pause Menu

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject PauseRoot;
+    [SerializeField] private Button ContinueButton;
+    [SerializeField] private Button QuitButton;
+
+    private void Start()
+    {
+        ContinueButton.onClick.AddListener(() =>
+        {
+            PauseRoot.SetActive(false);
+            Time.timeScale = 1f;
+        });
+        QuitButton.onClick.AddListener(() =>
+        {
+            PauseRoot.SetActive(false);
+            Time.timeScale = 1f;
+            SceneManager.LoadScene(0);
+        });
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseRoot.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+}
