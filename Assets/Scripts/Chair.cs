f0a5bfb7a0f13e624cd3ae9c87f1a197e6a89016
aleksat <aleksat@twodesperados.com>
2025-05-11 16:32:45 +0200

    Score not updating fixed

--- Assets/Scripts/Chair.cs
+++ Assets/Scripts/Chair.cs
@@ -91,6 +91,7 @@ public class Chair : MonoBehaviour, IInteractable
         Destroy(currectParticle);
         isInteracting = false;
         progressBar.gameObject.SetActive(false);
+        EventsNotifier.Instance.NotifyInteractionEnded(type, playerId);
         
         App.Instance.AudioManager.FinishInteraction(type);
     }
@@ -101,7 +102,6 @@ public class Chair : MonoBehaviour, IInteractable
         Destroy(actionTimer);
         Destroy(currectParticle);
         isInteracting = false;
-        EventsNotifier.Instance.NotifyInteractionEnded(type, playerId);
         // update visuals
         progressBar.gameObject.SetActive(false);
         App.Instance.AudioManager.FinishInteraction(type);
